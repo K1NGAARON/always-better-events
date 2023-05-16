5f1412f3c7a9a46a2bd3f3507de9b10f9846e7aa
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-16 09:51:33 +0200

    initial

---
+++ js/cards.js
@@ -0,0 +1,177 @@
+const content = [
+    {
+        title: "1. Apple Music Live returns for a brand-new season with Ed Sheeran",
+        tag: "onze-mensen",
+        category: "podcast",
+        img: "/posts/img/image1.jpg",
+        link: "dit-is-een-voorbeeld/",
+        date: "2023-05-05",
+    },
+    {
+        title: "2. Apple reports second quarter results",
+        tag: "brand-updates",
+        category: "podcast",
+        img: "/posts/img/image2.jpg",
+        link: "",
+        date: "2023-04-05",
+    },
+    {
+        title: "3. Apple launches 20 fun new games for its award-winning Apple Arcade service",
+        tag: "brand-updates",
+        category: "blog",
+        img: "/posts/img/image3.jpg",
+        link: "",
+        date: "2023-03-27",
+    },
+    {
+        title: "4. Apple, Google partner on an industry specification to address unwanted tracking",
+        tag: "onze-mensen",
+        category: "blog",
+        img: "/posts/img/image4.jpg",
+        link: "",
+        date: "2022-08-14",
+    },
+    {
+        title: "5. Apple Saket will open this Thursday, April 20, in New Delhi",
+        tag: "onze-mensen",
+        category: "podcast",
+        img: "/posts/img/image5.jpg",
+        link: "",
+        date: "2022-01-03",
+    },
+    {
+        title: "6. Apple Card's new high-yield Savings account is now available, offering a 4.15 percent APY",
+        tag: "welzijn-verhogen",
+        category: "blog",
+        img: "/posts/img/image6.jpg",
+        link: "",
+        date: "2023-05-06",
+    },
+    {
+        title: "7. Apple Music Live returns for a brand-new season with Ed Sheeran",
+        tag: "welzijn-verhogen",
+        category: "podcast",
+        img: "/posts/img/image1.jpg",
+        link: "post-1/",
+        date: "2023-05-05",
+    },
+    {
+        title: "8. Apple Saket will open this Thursday, April 20, in New Delhi",
+        tag: "leiderschap",
+        category: "podcast",
+        img: "/posts/img/image5.jpg",
+        link: "",
+        date: "2022-01-03",
+    },
+    {
+        title: "9. Apple launches 20 fun new games for its award-winning Apple Arcade service",
+        tag: "onze-mensen",
+        category: "blog",
+        img: "/posts/img/image3.jpg",
+        link: "",
+        date: "2023-03-27",
+    },
+    {
+        title: "10. Apple reports second quarter results",
+        tag: "leiderschap",
+        category: "podcast",
+        img: "/posts/img/image2.jpg",
+        link: "",
+        date: "2023-04-05",
+    },
+    {
+        title: "11. Apple, Google partner on an industry specification to address unwanted tracking",
+        tag: "leiderschap",
+        category: "blog",
+        img: "/posts/img/image4.jpg",
+        link: "",
+        date: "2022-08-14",
+    },
+    {
+        title: "12. Apple Saket will open this Thursday, April 20, in New Delhi",
+        tag: "welzijn-verhogen",
+        category: "podcast",
+        img: "/posts/img/image5.jpg",
+        link: "",
+        date: "2022-01-03",
+    },
+];
+
+const highlightedToShow = 6
+const archivedToShow = 8
+
+function sortPosts() {
+    content.sort((a, b) => new Date(b.date) - new Date(a.date));
+};
+
+
+function highlightedContent(e) {
+    const target = document.querySelector('#content');
+
+    if (target) {
+        const cards = content.slice(0, highlightedToShow).map(content => {
+            const tagWithSpaces = content.tag.replace(/-/g, ' ');
+
+            return `
+                <div class="card ${content.date} ${content.category} ${content.tag}">
+                    <div class="card-image">
+                        <img src="${content.img}" alt="${content.title}">
+                        <div class="overlay"></div>
+                    </div>
+                    <div class="card-content">
+                        <p class="tag">
+                            ${tagWithSpaces}
+                        </p>
+                        <h5>
+                            ${content.title}
+                        </h5>
+                        <a href="/posts/${content.link}" class="btn ghost no-border">
+                            Lees meer <i class="fa-solid fa-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            `;
+        }).join('');
+    
+        target.innerHTML = cards;
+    }
+};
+
+function archiveContent(e) {
+    const target = document.querySelector('#archive');
+
+    if (target) {
+        const cards = content.slice(highlightedToShow, highlightedToShow + archivedToShow).map(content => {
+            const tagWithSpaces = content.tag.replace(/-/g, ' ');
+
+            return `
+                <div class="card-wrapper">
+                    <img src="${content.img}" alt="${content.title}">
+                    <div class="card archive-card ${content.date} ${content.category} ${content.tag}">
+                        <div class="card-content">
+                            <p class="tag">
+                                ${tagWithSpaces}
+                            </p>
+                            <h5>
+                                ${content.title}
+                            </h5>
+                        </div>
+                        <a href="/posts/${content.link}" class="btn ghost no-border">
+                            Lees meer
+                        </a>
+                    </div>
+                </div>
+            `;
+    
+        }).join('');
+    
+        target.innerHTML = cards;
+    }
+};
+
+
+$(document).ready(function() {
+    sortPosts();
+    highlightedContent();
+    archiveContent();
+});
---
+++ posts/js/script.js
@@ -0,0 +1,169 @@
+let displayedPosts = 9;
+
+function loadMorePosts(e) {
+    e.preventDefault();
+
+    removeLastCardClass();
+
+    const target = document.querySelector('#allContent');
+    const remainingPosts = content.slice(displayedPosts);
+
+    if (remainingPosts.length > 0) {
+        const newCards = remainingPosts.slice(0, 9).map(content => {
+            const tagWithSpaces = content.tag.replace(/-/g, ' ');
+
+            return `
+                <div class="card all-cards ${content.date} ${content.category} ${content.tag}">
+                    <div class="card-image">
+                        <img src="${content.img}" alt="${content.title}">
+                        <div class="overlay"></div>
+                    </div>
+                    <div class="card-content">
+                        <p class="tag">
+                            ${tagWithSpaces}
+                        </p>
+                        <h5>
+                            ${content.title}
+                        </h5>
+                        <a href="/posts/${content.link}" class="btn ghost no-border">
+                            Lees meer <i class="fa-solid fa-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            `;
+        }).join('');
+    
+        target.insertAdjacentHTML('beforeend', newCards);
+        displayedPosts += 9;
+    }
+
+    addLastCardClass();
+    showLoadMoreButton();
+};
+
+function showLoadMoreButton() {
+    if (content.length > displayedPosts) {
+        $('.load-more').show();
+    } else {
+        $('.load-more').hide();
+    }
+};
+
+function removeLastCardClass(e) {
+    $('.card').removeClass('last-card');
+};
+
+function addLastCardClass(e) {
+    if (displayedPosts % 2 === 0) {
+        
+    } else {
+        $('.card').last().addClass('last-card');
+    }
+};
+
+function createAllContent(e) {
+    const target = document.querySelector('#allContent');
+
+    if (target) {
+        const cards = content.slice(0, displayedPosts).map(content => {
+            const tagWithSpaces = content.tag.replace(/-/g, ' ');
+
+            return `
+                <div class="card all-cards ${content.date} ${content.category} ${content.tag}">
+                    <div class="card-image">
+                        <img src="${content.img}" alt="${content.title}">
+                        <div class="overlay"></div>
+                    </div>
+                    <div class="card-content">
+                        <p class="tag">
+                            ${tagWithSpaces}
+                        </p>
+                        <h5>
+                            ${content.title}
+                        </h5>
+                        <a href="/posts/${content.link}" class="btn ghost no-border">
+                            Lees meer <i class="fa-solid fa-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            `;
+        }).join('');
+    
+        target.innerHTML = cards;
+    }
+
+    addLastCardClass();
+};
+
+function highlightedContent(e) {
+    const target = document.querySelector('#relatedContent');
+    
+    if (target) {
+        const cards = content.slice(0, 4).map(content => {
+            const tagWithSpaces = content.tag.replace(/-/g, ' ');
+
+            return `
+                <div class="card related-card ${content.date} ${content.category} ${content.tag}">
+                    <div class="card-image">
+                        <img src="${content.img}" alt="${content.title}">
+                        <div class="overlay"></div>
+                    </div>
+                    <div class="card-content">
+                        <p class="tag">
+                            ${tagWithSpaces}
+                        </p>
+                        <h5>
+                            ${content.title}
+                        </h5>
+                    </div>
+                    <a href="/posts/${content.link}" class="btn ghost no-border">
+                        Lees meer <i class="fa-solid fa-chevron-right"></i>
+                    </a>
+                </div>
+            `;
+        }).join('');
+    
+        target.innerHTML = cards;
+    }
+};
+
+function copyFacebook() {
+    // Get the current URL
+    let currentUrl = window.location.href;
+    
+    // Copy the URL to the clipboard
+    navigator.clipboard.writeText(currentUrl);
+    
+    // Open Facebook in a new tab with the URL in the post field
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(currentUrl), '_blank');
+};
+
+function copyTwitter() {
+    let url = encodeURIComponent(window.location.href);
+    let tweetUrl = 'https://twitter.com/intent/tweet?url=' + url;
+    window.open(tweetUrl, '_blank');
+};
+
+function copyLink() {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url)
+      .then(() => {
+        alert("URL copied to clipboard!");
+      })
+      .catch(() => {
+        alert("Failed to copy URL to clipboard");
+      });
+};
+
+
+$(document).ready(function() {
+    createAllContent();
+    highlightedContent();
+    showLoadMoreButton();
+
+    $('#loadMore').click(loadMorePosts);
+
+    $('#facebook').click(copyFacebook);
+    $('#twitter').click(copyTwitter);
+    $('#link').click(copyLink);
+});
